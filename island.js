d1c949fd5cc7d47abb548d47fd76ff400502dd8b
TheLove <lovepreetsinghcad@gmail.com>
2022-12-28 23:23:42 -0500

    island problem done

--- island.js
+++ island.js
@@ -1,7 +1,28 @@
 function getNeighbors(row, col, graph) {
 
   // Check top
-
+  let answer = [];
+  if((row-1) >= 0){
+  if(graph[row-1][col] == 1){
+    answer.push([row-1,col])
+  }
+}
+if((row+1) < graph.length){
+  if(graph[row+1][col] == 1){
+    answer.push([row+1,col])
+  }
+}
+if((col-1) >= 0){
+  if(graph[row][col-1] == 1){
+    answer.push([row,col-1])
+  }
+}
+if((col+1) < graph[row].length){
+  if(graph[row][col+1] == 1){
+    answer.push([row,col+1])
+  }
+}
+  return answer;
   // Check bottom
 
   // Check left
@@ -15,7 +36,26 @@ function getNeighbors(row, col, graph) {
 
 
 function islandSize(row, col, graph) {
+  let visited = new Set();
+  let queue = [];
+  let coord = [row,col];
+  let size = 0;
+  queue.push(coord);
+  visited.add(coord.toString());
+  while(queue.length !== 0){
+    let currentCoord = queue.shift();
+    size++;
+    let currentVal = graph[currentCoord[0]][currentCoord[1]];
 
+    let neighbours = getNeighbors(currentCoord[0],currentCoord[1],graph);
+    neighbours.forEach((el) =>{
+      if(visited.has(el.toString()) == false){
+        visited.add(el.toString());
+        queue.push(el);
+        }
+    })
+  }
+  return size;
   // Create a visited set to store visited nodes
 
   // Create a stack, put the starting node in the stack
